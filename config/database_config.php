3f3b6c83c1eb6bcee70ca1958535eb02e6b26f60
gustavohpk <ghpk88@gmail.com>
2014-06-27 21:17:34 -0300

    Adicionando gitignore e removendo database_config

--- config/database_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//Configurações para conexão com banco de dados.
-	$db_host = 'localhost';
-	$db_username = 'root';
-	$db_password = 'abc123';
-	$db_port = '5432';
-	$db_name = 'events';
-?>
