fd47702a17a0a00ec524e3e201a819d203f0fdd1
Rodrigo Miss <rodrigomiss@gmail.com>
2015-05-23 11:31:58 -0300

    implementação beta para emissão de crachás

--- config/database_config.php
+++ config/database_config.php
@@ -2,7 +2,7 @@
 	//Configurações para conexão com banco de dados.
 	$db_host = 'localhost';
 	$db_username = 'root';
-	$db_password = 'abc123';
+	$db_password = 'PI3,14@lolkk';
 	$db_port = '5432';
 	$db_name = 'events';
 ?>
